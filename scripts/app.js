f26a326a6600920caa6223d7846c526868f066d3
Marcos Paulo <82289818+mpaullos@users.noreply.github.com>
2023-03-20 10:37:01 -0300

    feat: animation script

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,16 @@
-alert("teste");
+const swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
