59aecd8b9d3fe65e74fb25a0b4ab86f3598c5758
Marcos Paulo <82289818+mpaullos@users.noreply.github.com>
2023-03-21 10:26:31 -0300

    feat: create function to change skills name

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,52 @@
 const swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+stack = ["HTML5", "CSS3", "Javascript", "Bootstrap", "NodeJs", "Python3", "MongoDB", "JQuery", "SQL"];
+$(".swiper-slide").click(function () {
+  $(".html5").click(function () {
+    $("#conhecimentos .teste").text(stack[0]);
   });
+
+  $(".css3").click(function () {
+    $("#conhecimentos .teste").text(stack[1]);
+  });
+
+  $(".js").click(function () {
+    $("#conhecimentos .teste").text(stack[2]);
+  });
+
+  $(".bootstrap").click(function () {
+    $("#conhecimentos .teste").text(stack[3]);
+  });
+
+  $(".nodejs").click(function () {
+    $("#conhecimentos .teste").text(stack[4]);
+  });
+  $(".python").click(function () {
+    $("#conhecimentos .teste").text(stack[5]);
+  });
+  $(".mongodb").click(function () {
+    $("#conhecimentos .teste").text(stack[6]);
+  });
+  $(".jquery").click(function () {
+    $("#conhecimentos .teste").text(stack[7]);
+  });
+
+  $(".sql").click(function () {
+    $("#conhecimentos .teste").text(stack[8]);
+  });
+});
