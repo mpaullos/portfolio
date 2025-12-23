0a3ab3a8d3f608b2620b7da97acfa0964876576d
Marcos Paulo <marcos.paullo32@gmail.com>
2025-12-23 16:23:01 -0300

    feat: add responsive navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Navbar } from "./components/Navbar";
+
 function App() {
   return (
-    <div className="min-h-screen bg-red-500 text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-red">Tailwind funcionando 🚀</h1>
+    <div className="bg-zinc-950 text-white">
+      <Navbar />
+
+      <main className="pt-16">
+        <section
+          id="home"
+          className="h-screen flex items-center justify-center"
+        >
+          <h1 className="text-4xl font-bold">Home</h1>
+        </section>
+      </main>
     </div>
   );
 }
