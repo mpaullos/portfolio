dffc18535e8e616a5c56dc2d0dccdc56b6eaf2f7
Marcos Paulo <marcos.paullo32@gmail.com>
2026-02-22 14:05:37 -0300

    feat: create marquee component

--- src/components/ui/marquee.tsx
+++ src/components/ui/marquee.tsx
@@ -1,27 +1,35 @@
-export default function Marquee({ items }: { items: string[] }) {
+export default function Marquee({
+  items,
+}: {
+  items: { icon: React.ReactNode; title: string }[];
+}) {
   return (
-    <div className="relative flex w-full overflow-x-hidden border-b-2 border-t-2 border-border bg-secondary-background text-foreground font-base">
-      <div className="animate-marquee whitespace-nowrap py-12">
-        {items.map((item) => {
-          return (
-            <span key={item} className="mx-4 text-4xl">
-              {item}
-            </span>
-          )
-        })}
+    <div
+      className="
+        relative flex w-full overflow-x-hidden
+        bg-[#85EEAB]
+        border-y-4 border-black
+        text-black font-black
+        
+      "
+    >
+      <div className="animate-marquee whitespace-nowrap py-10 flex">
+        {items.map((item, index) => (
+          <div key={index} className="mx-10 flex items-center gap-4 text-3xl">
+            <span className="text-4xl">{item.icon}</span>
+            <span>{item.title}</span>
+          </div>
+        ))}
       </div>
 
-      <div className="absolute top-0 animate-marquee2 whitespace-nowrap py-12">
-        {items.map((item) => {
-          return (
-            <span key={item} className="mx-4 text-4xl">
-              {item}
-            </span>
-          )
-        })}
+      <div className="absolute top-0 animate-marquee2 whitespace-nowrap py-10 flex">
+        {items.map((item, index) => (
+          <div key={index} className="mx-10 flex items-center gap-4 text-3xl">
+            <span className="text-4xl">{item.icon}</span>
+            <span>{item.title}</span>
+          </div>
+        ))}
       </div>
-
-      {/* must have both of these in order to work */}
     </div>
-  )
+  );
 }
