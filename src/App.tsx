64776a34a3b9a2a88cb742761f6b2eadd23f1181
Marcos Paulo <marcos.paullo32@gmail.com>
2026-02-22 14:06:07 -0300

    chore: change Navbar to main

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,11 @@ import Projects from "./sections/Projects";
 export default function App() {
   return (
     <div
-      className="min-h-screen bg-white dark:bg-zinc-950
+      className="min-h-screen 
                     text-zinc-900 dark:text-zinc-100 transition-colors"
     >
-      <Navbar />
-
       <main>
+        <Navbar />
         <Hero />
         <Projects />
       </main>
