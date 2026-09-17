9b269f0053a4eb6b0fd866a9564cd8fbcc263f51
Marcos Paulo <marcos.paullo32@gmail.com>
2026-09-17 18:57:18 -0300

    feat: install react router dom and implement new route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,31 @@ import Projects from "./sections/Projects";
 import Experience from "./sections/Experience";
 import Contact from "@/sections/Contact";
 import Footer from "@/sections/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Experience />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+function ProjectsPage() {
+  return (
+    <>
+      <Navbar />
+      <Projects showAll />
+      <Footer />
+    </>
+  );
+}
 
 export default function App() {
   return (
@@ -12,15 +37,14 @@ export default function App() {
       className="min-h-screen 
                     text-zinc-900 dark:text-zinc-100 transition-colors"
     >
-      <main>
-        <Navbar />
-        <Hero />
-        <About />
-        <Projects />
-        <Experience />
-        <Contact />
-        <Footer />
-      </main>
+      <BrowserRouter>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
